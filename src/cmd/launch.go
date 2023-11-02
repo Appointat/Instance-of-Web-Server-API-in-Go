c342f1951c5d76909789743f213735ba40493d95
partherope <125851906@qq.com>
2023-11-02 23:19:41 +0100

    Code done

---
+++ src/cmd/launch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"Instance_of_Web_Server_API_in_Go/src/server"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := server.NewServer()
+
+	http.HandleFunc("/ballot", server.HandleBallot)
+	http.HandleFunc("/vote", server.HandleVote)
+	http.HandleFunc("/result", server.HandleResult)
+
+	port := ":8080"
+	fmt.Printf("Server starting on port %s...\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
--- src/server/server.go
+++ src/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	modules "Instance_of_Web_Server_API_in_Go/src/types"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -19,6 +20,115 @@ type Ballot struct {
 	Winner   int
 }
 
+//method for ballot
+
+// rank returns the index of the alternative in one's preference list
+func (b Ballot) rank(alt int, VoterID string) int {
+	for i, pref := range b.Votes[VoterID] {
+		if pref == alt {
+			return i
+		}
+	}
+	return -1
+}
+
+// isPref func returns true if alt1 is preferred to alt2 by the voter with the given ID
+func (b Ballot) isPref(alt1, alt2 int, VoterID string) bool {
+	return b.rank(alt1, VoterID) < b.rank(alt2, VoterID)
+}
+
+func (b *Ballot) Majority() {
+	//count the number of votes for each alternative
+	count := make([]int, b.Alts)
+	for _, prefs := range b.Votes {
+		count[prefs[0]]++
+	}
+	//find the alternative with the most votes, if there is a tie, use the tie-break array.
+	//The alternative with the lowest index in the tie-break array wins
+	max := 0
+	for i := 1; i < b.Alts; i++ {
+		if count[i] > count[max] || (count[i] == count[max] && b.TieBreak[i] < b.TieBreak[max]) {
+			max = i
+		}
+	}
+	b.Winner = max
+}
+
+func (b *Ballot) Borda() {
+	//Borda rule: the alternative with the highest score wins
+	score := make([]int, b.Alts)
+	for _, prefs := range b.Votes {
+		for i, pref := range prefs {
+			score[pref] += b.Alts - i
+		}
+	}
+	//find the alternative with the highest score, if there is a tie, use the tie-break array.
+	max := 0
+	for i := 1; i < b.Alts; i++ {
+		if score[i] > score[max] || (score[i] == score[max] && b.TieBreak[i] < b.TieBreak[max]) {
+			max = i
+		}
+	}
+}
+
+func (b *Ballot) Condorcet() {
+	// Condorcet rule: the alternative that wins in all the pairwise comparisons wins
+	// create a matrix to store the pairwise comparison results
+	matrix := make([][]int, b.Alts)
+	for i := 0; i < b.Alts; i++ {
+		matrix[i] = make([]int, b.Alts)
+	}
+
+	// fill in the matrix
+	for i := 0; i < b.Alts; i++ {
+		for j := 0; j < b.Alts; j++ {
+			if i != j {
+				for _, voterID := range b.VoterIDs {
+					if b.isPref(i, j, voterID) {
+						matrix[i][j]++
+					}
+				}
+			}
+		}
+	}
+
+	// find the alternatives that win in all the pairwise comparisons
+	winners := []int{}
+	for i := 0; i < b.Alts; i++ {
+		win := true
+		for j := 0; j < b.Alts; j++ {
+			if i != j && matrix[i][j] <= matrix[j][i] {
+				win = false
+				break
+			}
+		}
+		if win {
+			winners = append(winners, i)
+		}
+	}
+
+	// handle cases
+	switch len(winners) {
+	case 0:
+		// no Condorcet winner
+		b.Winner = -1
+	case 1:
+		// a single Condorcet winner
+		b.Winner = winners[0]
+	default:
+		// multiple Condorcet winners, use the TieBreak array
+		b.Winner = winners[0]
+		for _, alt := range b.TieBreak {
+			for _, winner := range winners {
+				if winner == alt {
+					b.Winner = winner
+					return
+				}
+			}
+		}
+	}
+}
+
 type Server struct {
 	Ballots      map[string]Ballot
 	BallotNextID int //the next ballot ID to be assigned
@@ -30,7 +140,7 @@ func NewServer() *Server {
 	return &Server{make(map[string]Ballot), 0, 0}
 }
 
-func (s *Server) handleBallot(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandleBallot(w http.ResponseWriter, r *http.Request) {
 	//Analyse the request
 	var req modules.NewBallotRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -38,8 +148,8 @@ func (s *Server) handleBallot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//check if the rule is valid, here only Majority、Approval、Condorcet are allowed
-	validRules := []string{"Majority", "Approval", "Condorcet"}
+	//check if the rule is valid, here only Majority, Borda, Condorcet are allowed
+	validRules := []string{"Majority", "Borda", "Condorcet"}
 	valid := false
 	for _, rule := range validRules {
 		if req.Rule == rule {
@@ -87,8 +197,9 @@ func (s *Server) handleBallot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//affection of the ballot ID
-	ballotID := "scrutin" + string(s.BallotNextID)
+	//affection of the ballot ID, the ballot ID is "scrutin" + the next ballot ID(from number to string)
+	ballotID := fmt.Sprintf("scrutin%d", s.BallotNextID)
+
 	s.BallotNextID++
 	s.NumBallots++
 
@@ -113,7 +224,7 @@ func (s *Server) handleBallot(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func (s *Server) handleVote(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandleVote(w http.ResponseWriter, r *http.Request) {
 	var req modules.VoteRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
@@ -184,4 +295,47 @@ func (s *Server) handleVote(w http.ResponseWriter, r *http.Request) {
 	//return 200
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("vote pris en compte"))
+}
+
+func (s *Server) HandleResult(w http.ResponseWriter, r *http.Request) {
+	var req modules.ResultRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	//check if the ballot ID is valid, return 404 if not
+	ballot, ok := s.Ballots[req.BallotID]
+	if !ok {
+		http.Error(w, "ballot ID not found", http.StatusNotFound)
+		return
+	}
+
+	//check if the result is ready, if not return 425
+	//if there are no votes, the result is not ready
+	if len(ballot.Votes) == 0 {
+		http.Error(w, "result not ready", 425)
+		return
+	}
+	//Calculate the result
+	switch ballot.Rule {
+	case "Majority":
+		ballot.Majority()
+	case "Borda":
+		ballot.Borda()
+	case "Condorcet":
+		ballot.Condorcet()
+	}
+	s.Ballots[req.BallotID] = ballot
+	var response modules.ResultResponse
+	if ballot.Winner == -1 {
+		response = modules.ResultResponse{}
+	} else {
+		response = modules.ResultResponse{
+			Winner:  ballot.Winner,
+			Ranking: []int{},
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
